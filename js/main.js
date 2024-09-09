9d0ccfba05b9e9b0f1e690bd34e96d0455212505
LegionAro <u@example.com>
2024-09-09 18:51:29 +0300

    edit description block,add modal

--- js/main.js
+++ js/main.js
@@ -167,3 +167,40 @@ headerScroll();
 window.addEventListener('scroll', function () {
   headerScroll();
 });
+
+//Modal
+function isModal() {
+  let modalBtns = document.querySelectorAll('.modal__btn');
+
+  if (modalBtns.length > 0) {
+    for (let modalBtn of modalBtns) {
+      modalBtn.addEventListener('click', function () {
+        let modalBtnData = modalBtn.getAttribute('data-modal-src');
+        let modalWindow = document.querySelector(`*[data-modal-window="${modalBtnData}"]`);
+        let body = document.querySelector('body');
+
+        if (modalWindow) {
+          modalWindow.classList.add('active');
+          body.classList.add('lock');
+        }
+      });
+    }
+  }
+}
+isModal();
+
+function isModalClose() {
+  let modalCloseBtns = document.querySelectorAll('.modal__btn-close');
+  if (modalCloseBtns.length > 0) {
+    for (let modalCloseBtn of modalCloseBtns) {
+      modalCloseBtn.addEventListener('click', function () {
+        let modalWindow = modalCloseBtn.closest('*[data-modal-window]');
+        let body = document.querySelector('body');
+
+        modalWindow.classList.remove('active');
+        body.classList.remove('lock');
+      });
+    }
+  }
+}
+isModalClose();
